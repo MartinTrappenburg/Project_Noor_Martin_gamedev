3dd9c26e32a7361644e82bca3d6faa01847bf6e6
noor chamlal <331756@student.mboutrecht.nl>
2021-06-23 05:24:17 +0200

    python versie

---
+++ game/py/main.py
@@ -0,0 +1,51 @@
+def main():
+
+    #storyline starts
+    print ("you are a software developer working in your bedroom.
+    print ("two years ago you started a company from home.")
+    print ("it hasnt been doing well and your motivation is slowly starting to disappear.")
+    print ("everyday is the same, wake up, spend time on your computer,go to bed. ")
+    print ("recently, youve found a new online game that is rising fame."):
+
+    #first quest
+    answer = input("would you like to play? (yes/no)")
+
+    if answer = "yes"
+        print("good choice! +1 skillpoint")
+    else ==
+        print("we're gonna make you play either way")
+
+    #storyline starts again
+    print("this game seems like any other game, nothing special. you have met some new friends in this game tho")
+    print ("jerry, kyle and thomas. you've told them about your small business and they gave you some tips.")
+    print ("they have even offered to help")
+
+    #second quest
+    answer = input ("will you accept their help? (yes/no)")
+
+    if answer = "yes"
+        print ("+1 skillpoint")
+    else ==
+        print ("you're losing points smh")
+
+    #storyline
+    print ("you've decided to let your new online friends help you. their advice has worked and your business is doing better.")
+    print ("but working online hasnt been easy, so you decide to invite everyone to your place.")
+    print ("befor that happens you will have to upgrade your place.")
+
+    #third quest
+    answer = input("will you decide to upgrade your place? (yes/no)")
+
+    if answer = input "yes"
+        print ("valid")
+    else == 
+        print ("-1 skillpoint")
+
+    #storyline
+    print("you're moving from your bedroom to the garage. you have put 4 desks and monitors ready to be used.")
+
+
+    
+            
+
+	
